daa8efeee18e43d7c33caa0b699c873b11eda10a
ashishrahi <148324855+ashishrahi@users.noreply.github.com>
2025-07-12 17:38:41 +0530

    updated code

--- server.js
+++ server.js
@@ -5,12 +5,13 @@ import cron from 'node-cron';
 import importLogRoutes from './src/routes/importLogRoutes.js';
 import { fetchJobsFromFeeds } from './src/services/JobService.js';
 import dbConnect from './src/config/dbConnect.js';
+import cors from 'cors'
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 dbConnect()
-
+app.use(cors())
 
 app.use('/v1/api', importLogRoutes);
 
